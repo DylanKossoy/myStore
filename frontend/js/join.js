47f396d5da034adbd21d36f15db6cf74b01c0cef
DylanKossoy <dylanakossoy@gmail.com>
2025-08-03 14:11:37 -0400

    update on join

--- frontend/js/join.js
+++ frontend/js/join.js
@@ -1,3 +1,40 @@
+import { triggerShake } from './utils.js'
 
 
 
+
+
+function validateInputs({username, email, password, confirmPassword}) {
+    if(!username || !email || !password || !confirmPassword) {
+    
+        return '*All fields need to be filled out*'
+    }
+
+
+ 
+}
+
+
+
+function showError(error) {
+    let errorText  = document.querySelector('.error-message-text');
+    errorText.textContent = error
+}
+
+
+
+function clearError() {
+    document.querySelector('.error-message-text').textContent = ''
+}
+
+
+
+
+
+async function handleSubmit(e) {
+    e.preventDefault();
+
+
+
+    
+}
