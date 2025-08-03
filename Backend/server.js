7ebefe633766f96fc54e2649f92fb664d8e95bf3
DylanKossoy <dylanakossoy@gmail.com>
2025-08-02 22:21:00 -0400

    update on pc end

--- Backend/server.js
+++ Backend/server.js
@@ -11,8 +11,8 @@ app.use(cors())
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('✅ MongoDB connected'))
-  .catch(err => console.error('❌ MongoDB connection error:', err))
+  .then(() => console.log(' MongoDB connected'))
+  .catch(err => console.error(' MongoDB connection error:', err))
 
 app.use('/api/users', userRoutes)
 
