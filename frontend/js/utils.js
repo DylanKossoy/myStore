844c796deabf12fa6d338a02415f9e9dc0cf25a4
DylanKossoy <dylanakossoy@gmail.com>
2025-08-03 12:35:30 -0400

    more

---
+++ frontend/js/utils.js
@@ -0,0 +1,15 @@
+
+
+
+
+// function to trigger shake animation on input field with a little interval timer 
+
+
+export function triggerShake(field) {
+    field.classList.add('shake')
+
+    setInterval(() => {
+        field.classList.remove('shake')
+
+    },500)
+}
