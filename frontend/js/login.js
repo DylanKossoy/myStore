c684537f1a5be7d28f25384700968411569ec226
Dylan Kossoy <dylankossoy@Mac.lan>
2025-08-04 20:34:07 -0400

    update login form frontend

---
+++ frontend/js/login.js
@@ -0,0 +1,13 @@
+
+
+function clearError() {
+    document.querySelector()
+
+    
+}
+
+
+function showError(error) {
+    let errorText = document.querySelector('.login-error-message')
+    errorText.textContent = error
+}
