ba88dd9891da95f72db6ce77d506bf390fe4c56a
Dylan Kossoy <dylankossoy@Mac.lan>
2025-08-04 20:47:41 -0400

    update login a little

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,7 +1,35 @@
+import { triggerShake } from './utils.js'
+
+
+
+let usernameInput = document.querySelector('.login-input-username')
+let passwordInput = document.querySelector('.login-input-password');
+
+
+function validateLogin({ username, password }) {
+    let error = false
+
+
+    if(!username) {
+        triggerShake(usernameInput)
+        
+        error = true;
+    }
+
+    if(!password) {
+        triggerShake(passwordInput) 
+        error = true
+    }
+    
+    if(error) {
+        return '* Fill out all fields *'
+    }
+
+
 
 
 function clearError() {
-    document.querySelector()
+    document.querySelector('.login-error-message').textContent = ''
 
     
 }
@@ -10,4 +38,17 @@ function clearError() {
 function showError(error) {
     let errorText = document.querySelector('.login-error-message')
     errorText.textContent = error
+}
+
+
+
+function handleLogin(e) {
+    e.preventDefault()
+
+
+    let errorMessage = validateLogin(usernameInput.value)
+
+
+
+
 }
